03acdd687446499ae41033b3359185f872eedc1e
Doug Hagan <dougecko@users.noreply.github.com>
2021-10-17 22:32:43 +1100

    convert to Spring Boot application

---
+++ src/test/java/shine/aba/AbaApplicationTests.java
@@ -0,0 +1,13 @@
+package shine.aba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
